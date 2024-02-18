50061533c4a7907422b07fe2826c220c2d7ec4e8
farahyasserr <farah.dawoudd@gmail.com>
2024-02-18 15:30:40 +0400

    feat: Add colors shared file for theme

---
+++ src/theme/Colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    primary: '#F56960',
+    secondary: '#FFC267',
+    lightGray:  '#d3d3d3',
+    black: '#000000',
+    white: '#ffffff',
+    gray: '#808080',
+    transparent:'transparent',
+  };
