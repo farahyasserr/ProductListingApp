9d9aead5dcf7969ad89f923d1a1f68aa9b653d88
farahyasserr <farah.dawoudd@gmail.com>
2024-02-18 17:15:25 +0400

    refactor(screens): Add index file for screens

---
+++ src/screens/index.ts
@@ -0,0 +1,4 @@
+export { default as HomeScreen } from './HomeScreen/HomeScreen.container'
+export { default as ProductDetailsScreen } from './ProductDetailsScreen/ProductDetailsScreen.container'
+export { default as UpdateProductScreen } from './UpdateProductScreen/UpdateProductScreen.container'
+
