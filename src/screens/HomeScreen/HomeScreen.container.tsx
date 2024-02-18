4c8566f4719d87b8dc55596cad6124f8d1b3e06e
farahyasserr <farah.dawoudd@gmail.com>
2024-02-18 17:13:36 +0400

    feat(ProductList): Create Home Screen to render Product List
    - Integrated with backend endpoint
    - Styles done

---
+++ src/screens/HomeScreen/HomeScreen.container.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import { Alert } from 'react-native'
+import HomeScreenView from './HomeScreen.view'
+import { useGetProductsQuery } from '../../services/product/productApi';
+import { deleteProduct, setProducts } from '../../store/reducers/ProductsSliceReducer';
+import { HomeStackNavType } from '../../navigation/Home/HomeStack';
+import { useNavigation } from '@react-navigation/native'
+import { useDispatch, useSelector } from 'react-redux';
+import { selectProducts } from '../../store/selectors/ProductsSelector';
+import Product from '../../types/Product';
+
+function HomeScreen() {
+   const dispatch = useDispatch();
+    /* `const { data , isLoading } = useGetProductsQuery();` is using the `useGetProductsQuery` hook
+    from the `productApi` service to fetch data from the server. */
+    const { data , isLoading } = useGetProductsQuery();
+
+
+    useEffect(() => {
+      if (data) {
+          dispatch(setProducts(data)); // Dispatch setProducts action with the fetched data
+      }
+  }, [data, dispatch]);
+
+
+    const navigation = useNavigation<HomeStackNavType<'Home'>>();
+
+    const deleteProductHandler = (id: number)=>{
+      console.log(" hsiuhowidhknn", id)
+      dispatch(deleteProduct(id))
+    }
+
+    const addToCartHandler = ()=>{
+      Alert.alert("Coming soon!")
+    }
+
+
+    const pressItemhandler = (product: Product) => {
+        navigation.navigate('ProductDetails', { productItem: product });
+      };
+
+    const products = useSelector(selectProducts); // Fetch products from the Redux store
+
+  return <HomeScreenView products={products} isLoading={isLoading} addToCartHandler={addToCartHandler} deleteProductHandler={deleteProductHandler} pressItemhandler={pressItemhandler} />;
+}
+
+export default HomeScreen;
---
+++ src/screens/HomeScreen/HomeScreen.view.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Text, View, FlatList } from 'react-native'
+import Product from '../../types/Product';
+import Loader from '../../components/shared/Loader/Loader.view';
+import styles from './HomeScreen.styles';
+import { EmptyList, ProductItem } from './blocks';
+import { colors } from '../../theme/Colors';
+
+interface Props {
+  isLoading: Boolean
+  products?: Product[]
+  deleteProductHandler: (id: number)=>void
+  addToCartHandler: ()=> void
+  pressItemhandler: ( product: Product )=> void
+}
+
+function HomeScreenView({isLoading, products, addToCartHandler, deleteProductHandler, pressItemhandler}: Props) {
+  return (
+    <View style={{backgroundColor: colors.white, height: '100%'}}>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        false?
+        <EmptyList />
+        :
+        <View style={{paddingHorizontal: 14, backgroundColor: colors.white, height: '100%'}}>
+        <FlatList
+          data={products}
+          renderItem={({item}) => <ProductItem item={item} deleteProductHandler={deleteProductHandler} addToCartHandler={addToCartHandler} pressItemhandler={pressItemhandler}/>}
+          keyExtractor={item => item.id.toString()}
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={() => (
+            <Text style={styles.listHeaderText}>Products</Text>
+          )}
+        />
+        </View>
+      )}
+    </View>
+  );
+}
+
+export default HomeScreenView;
