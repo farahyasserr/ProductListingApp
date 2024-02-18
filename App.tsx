cd20b7f983fc9336bd5371e030b32b7b894bf131
farahyasserr <farah.dawoudd@gmail.com>
2024-02-18 13:51:19 +0400

    chore: initial commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigator from './src/navigation';
+import { Text } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './src/store';
+
+function App(): React.JSX.Element {
+
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <MainNavigator />
+        <Text>testtttt</Text>
+      </NavigationContainer>
+    </Provider >
+
+  );
+}
+
+
+export default App;
