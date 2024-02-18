5fb08f3bf2220365998bfaa35592652eeaa807ce
farahyasserr <farah.dawoudd@gmail.com>
2024-02-19 00:43:02 +0400

    feat(store): Add Selected Item to store

--- src/store/reducers/ProductsSliceReducer.ts
+++ src/store/reducers/ProductsSliceReducer.ts
@@ -2,11 +2,13 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import Product from '../../types/Product';
 
 interface ProductsState {
-  items: Product[];
+  items?: Product[];
+  selectedProduct?: Product;
 }
 
 const initialState: ProductsState = {
-  items: [],
+  items: undefined,
+  selectedProduct: undefined,
 };
 
 const productsSlice = createSlice({
@@ -17,18 +19,25 @@ const productsSlice = createSlice({
       state.items = action.payload;
     },
     deleteProduct(state, action: PayloadAction<number>) {
-      state.items = state.items.filter(product => product.id !== action.payload);
+      state.items = state.items!.filter(
+        product => product.id !== action.payload,
+      );
     },
     updateProduct(state, action: PayloadAction<Product>) {
       const item = action.payload;
-      const index = state.items.findIndex(product => product.id === item.id);
-      if (index !== -1) {
+      const index = state.items!.findIndex(product => product.id === item.id);
+      if (index !== -1 && state.items) {
         state.items[index] = item;
+        state.selectedProduct = item;
       }
+    },
+    setSelectedProduct(state, action: PayloadAction<Product>) {
+      state.selectedProduct = action.payload;
     },
   },
 });
 
-export const { setProducts, deleteProduct , updateProduct} = productsSlice.actions;
+export const { setProducts, deleteProduct, updateProduct, setSelectedProduct } =
+  productsSlice.actions;
 
 export default productsSlice.reducer;
--- src/store/selectors/ProductsSelector.ts
+++ src/store/selectors/ProductsSelector.ts
@@ -1,3 +1,5 @@
 import { RootState } from '../reducers/index';
 
 export const selectProducts = (state: RootState) => state.products.items;
+export const selectedProduct = (state: RootState) =>
+  state.products.selectedProduct;
