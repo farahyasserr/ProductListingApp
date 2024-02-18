6718c12f6322432475706d5d68c14a766e728cbf
farahyasserr <farah.dawoudd@gmail.com>
2024-02-18 22:15:01 +0400

    feat: Inject endpoints for update, delete, and get product details

--- src/services/product/productApi.ts
+++ src/services/product/productApi.ts
@@ -1,18 +1,41 @@
-import Product from "../../types/Product";
-import { apiInstance } from "..";
+import Product from '../../types/Product';
+import { apiInstance } from '..';
 
 export const productsApi = apiInstance.injectEndpoints({
-    endpoints: (builder) => ({
-       /* The `getProducts` property is defining an endpoint for retrieving the list of products. */
-        getProducts: builder.query<Product[], void>({
-            query: () => ({
-                url: '/products',
-                method: 'GET',
-            }),
-        })
+  endpoints: builder => ({
+    /* The `getProducts` property is defining an endpoint for retrieving the list of products. */
+    getProducts: builder.query<Product[], void>({
+      query: () => ({
+        url: '/products',
+        method: 'GET',
+      }),
     }),
-    overrideExisting: false,
-  })
+    getProductDetails: builder.query<Product, number>({
+      query: productId => ({
+        url: `/products/${productId}`,
+        method: 'GET',
+      }),
+    }),
+    deleteProduct: builder.query<Product, number>({
+      query: productId => ({
+        url: `/products/${productId}`,
+        method: 'DELETE',
+      }),
+    }),
+    updateProduct: builder.query<Product, number>({
+      query: productId => ({
+        url: `/products/${productId}`,
+        method: 'PUT',
+      }),
+    }),
+  }),
+  overrideExisting: false,
+});
 
-  //Export hooks for usage in function components
-  export const { useGetProductsQuery } = productsApi
+//Export hooks for usage in function components
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useDeleteProductQuery,
+  useUpdateProductQuery,
+} = productsApi;
